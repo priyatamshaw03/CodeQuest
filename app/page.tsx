ea5012d686b473466be8c66e90af229385092a9c
Priyatam Shaw <priyatamshaw@gmail.com>
2026-01-17 12:07:40 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-game">welcome to this project</h1>
+      <Button>subscribe</Button>
+    </div>
+  );
+}
